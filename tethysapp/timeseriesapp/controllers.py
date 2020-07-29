26154fff429b37dffdace4a2f2c6d37492e7699c
Elkin Romero <gio.busrom@gmail.com>
2020-07-29 16:27:28 -0600

    csv option finished

--- tethysapp/timeseriesapp/controllers.py
+++ tethysapp/timeseriesapp/controllers.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from tethys_sdk.permissions import login_required
 from tethys_sdk.gizmos import Button
 from .app import Timeseriesapp as app
+import glob, os, datetime
+import pandas as pd
+from django.http import JsonResponse
 
 @login_required()
 def home(request):
@@ -44,3 +47,82 @@ def geoserver_(request):
     }
 
     return render(request, 'timeseriesapp/geoserver.html', context)
+
+#UPLOAD A CSV ##
+def delete_old_observations():
+    workspace_path = app.get_app_workspace().path
+    uploaded_observations = glob.glob(os.path.join(workspace_path, 'observations', '*.csv'))
+    expiration_time = datetime.datetime.now() - datetime.timedelta(days=1)
+    for uploaded_observation in uploaded_observations:
+        created_date = datetime.datetime.fromtimestamp(os.path.getctime(uploaded_observation))
+        if created_date <= expiration_time:
+            os.remove(uploaded_observation)
+    return
+
+
+def list_uploaded_observations():
+    workspace_path = app.get_app_workspace().path
+    uploaded_observations = glob.glob(os.path.join(workspace_path, 'observations', '*.csv'))
+    list_of_observations = []
+    for uploaded_observation in uploaded_observations:
+        file_name = os.path.basename(uploaded_observation)
+        presentation_name = file_name.replace('_', ' ').replace('.csv', '')
+        list_of_observations.append((presentation_name, file_name))
+    return tuple(sorted(list_of_observations))
+
+
+def upload_new_observations(request):
+    print("we are in the uploade_new_observations")
+    delete_old_observations()
+    workspace_path = app.get_app_workspace().path
+    files = request.FILES
+    print(files)
+    responseObj={}
+    for file in files:
+        # new_observation_path = os.path.join(workspace_path, 'observations', files[file].name)
+        new_observation_path = os.path.join(workspace_path, files[file].name)
+        with open(new_observation_path, 'wb') as dst:
+            for chunk in files[file].chunks():
+                dst.write(chunk)
+        try:
+            df = pd.read_csv(new_observation_path)
+            df.dropna(inplace=True)
+            print(df)
+        except Exception as e:
+            JsonResponse(dict(error='Cannot read the csv provided. It may not be a valid csv file.'))
+        try:
+            df.datetime = pd.to_datetime(df.datetime)
+            # df = df.resample('D', axis=0).mean()
+            print(df)
+
+        except Exception as e:
+            JsonResponse(dict(error='Unable to recognize dates. Please specify 1 date/streamflow value pair per date. '
+                                    'Recommended datetime format is YYYY-MM-DD HH:MM:SS'))
+        try:
+            print("hola")
+            print(responseObj)
+            # df.datetime = pd.to_datetime(df.datetime).tz_localize('UTC')
+            print(df)
+            dates = df['datetime'].tolist()
+            values = df['streamflow (m^3/s)'].tolist()
+            stationsIDs= df['stationID'].tolist()
+            lats = df['lat'].tolist()
+            longs = df['long'].tolist()
+            # print(dates)
+            # print(values)
+            # print(stationsIDs)
+            # print(lats)
+            # print(longs)
+            responseObj['dates']=dates
+            responseObj['values'] = values
+            responseObj['stationsIDs'] = stationsIDs
+            responseObj['lats'] = lats
+            responseObj['longs'] = longs
+            print(responseObj)
+        except Exception as e:
+            pass
+
+        df.to_csv(new_observation_path)
+
+    # return JsonResponse(dict(new_file_list=list_uploaded_observations()))
+    return JsonResponse(responseObj)
---
+++ tethysapp/timeseriesapp/public/js/csv.js
@@ -0,0 +1,149 @@
+// INIT MAP OF THE CSV PAGE //
+
+var center = [4.5709, -74.2973];
+var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+var osm = L.tileLayer(osmUrl, { maxZoom: 18, attribution: osmAttrib });
+var map2 = new L.Map('map', { center: center, zoom: 5 });
+osm.addTo(map2);
+
+/*
+****** FUNCTION NAME: addDefaultBehaviorToAjax *********
+****** FUNCTION PURPOSE: make dynamic ajax requests *********
+*/
+addDefaultBehaviorToAjax = function() {
+    // Add CSRF token to appropriate ajax requests
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!checkCsrfSafe(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", getCookie("csrftoken"))
+            }
+        }
+    })
+}
+/*
+****** FU1NCTION NAME: checkCsrfSafe *********
+****** FUNCTION PURPOSE: CHECK THE OPERATIONS THAT DOES NOT NEED A CSRF VERIFICATION *********
+*/
+checkCsrfSafe = function(method) {
+    // these HTTP methods do not require CSRF protection
+    return /^(GET|HEAD|OPTIONS|TRACE)$/.test(method)
+}
+/*
+****** FU1NCTION NAME: getCookie *********
+****** FUNCTION PURPOSE: Retrieve a cookie value from the csrf token *********
+*/
+getCookie = function(name) {
+    var cookie
+    var cookies
+    var cookieValue = null
+    var i
+    if (document.cookie && document.cookie !== "") {
+        cookies = document.cookie.split(";")
+        for (i = 0; i < cookies.length; i += 1) {
+            cookie = $.trim(cookies[i])
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === name + "=") {
+                cookieValue = decodeURIComponent(
+                    cookie.substring(name.length + 1)
+                )
+                break
+            }
+        }
+    }
+    return cookieValue
+}
+//ACTIVATE THE DEFAULT BEHAVIOR OF THE AJAX FOR POST REQUEST //
+addDefaultBehaviorToAjax();
+
+////////////////////////////////////////////////////////////////////////  UPLOAD OBSERVATIONAL DATA
+$("#hydrograph-csv-form").on('submit', function (e) {
+    e.preventDefault();
+    let upload_stats = $("#hydrograph-csv-status");
+    upload_stats.html('in progress...')
+    console.log(new FormData(this));
+    console.log(URL_upload_new_observations);
+    $.ajax({
+        type: 'POST',
+        url: URL_upload_new_observations,
+        // url: 'upload-new-observations/',
+        data: new FormData(this),
+        dataType: 'json',
+        contentType: false,
+        cache: false,
+        processData: false,
+        success: function (response) {
+            console.log(response);
+            upload_stats.html('uploaded finished successfully.')
+            let uploaded_input = $("#uploaded_observations");
+            let stationsID= response['stationsIDs'];
+            let datess= response['dates'];
+            let lats= response['lats'];
+            let longs= response['longs'];
+            let values= response['values'];
+            let uniqueStationsID = [...new Set(stationsID)];
+            let uniquelats = [...new Set(lats)];
+            let uniquelongs = [...new Set(longs)];
+            let timeSeriesObject=[]
+            for(let j=0; j< uniqueStationsID.length; ++j){
+              let timeSerieStation={};
+              timeSerieStation['stationID']=uniqueStationsID[j];
+              timeSerieStation['lat']=uniquelats[j];
+              timeSerieStation['long']=uniquelongs[j];
+              let timesArray=[];
+              let valuesArray = [];
+              for(let i=0; i< values.length; ++i){
+                if(uniqueStationsID[j] == stationsID[i]){
+                  timesArray.push(datess[i]);
+                  valuesArray.push(values[i]);
+                }
+              }
+              timeSerieStation['values']= valuesArray;
+              timeSerieStation['dates']=timesArray;
+              timeSeriesObject.push(timeSerieStation);
+            }
+
+            console.log(timeSeriesObject);
+
+
+            for(let i=0; i< uniqueStationsID.length; ++i){
+              let latlong = [uniquelats[i], uniquelongs[i]];
+              console.log(latlong);
+              let marker =  L.marker(latlong).addTo(map2).on('click', function(e){
+                console.log(e);
+                timeSeriesObject.forEach(function(x){
+                  if(x['lat']==e['latlng']['lat']){
+                    var timeserie1 = {
+                      x: x['dates'],
+                      y: x['values'],
+                      mode: 'lines',
+                      name: 'Lines'
+                    };
+
+
+                    var data = [timeserie1];
+
+                    var layout = {
+                      title: x['stationID'],
+                      xaxis: {
+                        title: 'Streamflow (cms)'
+                      },
+                      yaxis: {
+                        title: 'Date'
+                      }
+                    };
+                    $("#ghs").empty();
+                    Plotly.newPlot('ghs', data, layout);
+                  }
+                })
+
+              });
+            }
+
+
+        },
+        error: function (response) {
+            upload_stats.html('failed to upload. ' + response['error'])
+        },
+    });
+});
